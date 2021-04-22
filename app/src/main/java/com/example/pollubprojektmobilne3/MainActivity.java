fee063f9061865d5e8dedaf810278388dd57de52
PanterA989 <krzysztof.kania1@pollub.edu.pl>
2021-04-22 10:40:50 +0200

    Updated main activity layout and added list element view

--- app/src/main/java/com/example/pollubprojektmobilne3/MainActivity.java
+++ app/src/main/java/com/example/pollubprojektmobilne3/MainActivity.java
@@ -3,12 +3,53 @@ package com.example.pollubprojektmobilne3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.listOfSmartphones);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("JEDEN");
+        arrayList.add("DWA");
+        arrayList.add("TRZY");
+        arrayList.add("CZTERY");
+        arrayList.add("PIĘĆ");
+        arrayList.add("SZEŚĆ");
+        arrayList.add("SIEDEM");
+        arrayList.add("OSIEM");
+        arrayList.add("DZIEWIĘĆ");
+        arrayList.add("DZIESIĘĆ");
+        arrayList.add("JEDENAŚCIE");
+        arrayList.add("DWANAŚCIE");
+        arrayList.add("JEDEN");
+        arrayList.add("DWA");
+        arrayList.add("TRZY");
+        arrayList.add("CZTERY");
+        arrayList.add("PIĘĆ");
+        arrayList.add("SZEŚĆ");
+        arrayList.add("SIEDEM");
+        arrayList.add("OSIEM");
+        arrayList.add("DZIEWIĘĆ");
+        arrayList.add("DZIESIĘĆ");
+        arrayList.add("JEDENAŚCIE");
+        arrayList.add("DWANAŚCIE");
+        if(arrayList.isEmpty()) {
+            findViewById(R.id.noDataTextView).setVisibility(View.VISIBLE);
+        }
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+
+        listView.setAdapter(arrayAdapter);
     }
 }
